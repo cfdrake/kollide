32f8d48b55f114ebee4f0aef41ea417e4e0080e2
Colin Drake <github@accounts.colindrake.me>
2021-10-22 10:21:06 +0700

    Code cleanup

--- kalos.lua
+++ kalos.lua
@@ -1,48 +1,18 @@
--- kalos: k-loop clone
+-- kalos: k-loop-alike
 -- by: @cfd90
 --
 -- K2 play sample 1
 -- K3 play sample 2
 -- E1 rate
--- E2 lfo rate
+-- E2 lfo speed
 -- E3 lfo amount
 
--- TODO:
--- K1 + K2 and K1 + K3 to load sample 3 and sample 4
--- get psets working
--- maybe increase size of LFO in rate animation
-
-local rate = 1
-local lfo_rate = 0.05
+-- Variables for calculating LFO.
 local lfo_val = 0
-local lfo_amount = 0
-local c = 0
-
-local sample = -1
-local file1 = ""
-local file2 = ""
+local lfo_counter = 0
 
 function init()
-  params:add_separator()
-  
-  params:add_file("sample 1", "sample 1")
-  params:set_action("sample 1", function(file)
-    local was_empty = file1 == "" and file2 == ""
-    file1 = file
-    if was_empty then
-      load_file(1)
-    end
-  end)
-  
-  params:add_file("sample 2", "sample 2")
-  params:set_action("sample 2", function(file)
-    local was_empty = file1 == "" and file2 == ""
-    file2 = file
-    if was_empty then
-      load_file(2)
-    end
-  end)
-
+  -- Initialize softcut.
   softcut.buffer_clear()
   softcut.enable(1, 1)
   softcut.buffer(1, 1)
@@ -54,12 +24,32 @@ function init()
   softcut.rate(1, 1)
   softcut.play(1, 1)
   
+  -- Set up params.
+  params:add_separator()
+  params:add_file("sample 1", "sample 1")
+  params:set_action("sample 1", function(filename)
+    load_file(filename)
+  end)
+  
+  params:add_file("sample 2", "sample 2")
+  params:set_action("sample 2", function(filename)
+    load_file(filename)
+  end)
+  
+  params:add{id="rate", name="pitch", type="control", controlspec=controlspec.new(-3.0, 3.0, 'lin', 0.01, 1, "", 1/500)}
+  params:add{id="lfo_rate", name="lfo speed", type="control", controlspec=controlspec.new(0.01, 0.5, 'lin', 0.01, 0.05, "", 1/500)}
+  params:add{id="lfo_amount", name="lfo amount", type="control", controlspec=controlspec.new(0, 1, 'lin', 0.01, 0, "", 1/500)}
+
+  -- Set up LFO timer.
   lfo_metro = metro.init()
   lfo_metro.time = .01
   lfo_metro.event = function()
-    lfo()
+    update_lfo()
   end
   
+  lfo_metro:start()
+  
+  -- Set up UI timer.
   ui_metro = metro.init()
   ui_metro.time = 1/30
   ui_metro.event = function()
@@ -67,96 +57,82 @@ function init()
   end
   
   ui_metro:start()
-  lfo_metro:start()
 end
 
-function load_file(n)
-  file = ""
-  sample = n
-  
-  if n == 1 then
-    file = file1
-  else
-    file = file2
-  end
-    
-  -- softcut.play(1, 0)
+function load_file(filename)
   softcut.buffer_clear()
-  softcut.buffer_read_mono(file, 0, 1, -1, 1, 1)
+  softcut.buffer_read_mono(filename, 0, 1, -1, 1, 1)
   softcut.enable(1, 1)
   softcut.buffer(1, 1)
   softcut.level(1, 1)
   softcut.loop(1, 1)
   softcut.loop_start(1, 1)
-  softcut.loop_end(1, file_length(file))
+  softcut.loop_end(1, file_length(filename))
   softcut.position(1, 1)
-  -- softcut.rate(1, 1)
   softcut.play(1, 1)
 end 
 
-function lfo()
-  c = c + lfo_rate
-  lfo_val = lfo_amount * math.sin(c)
-  softcut.rate(1, rate + lfo_val)
+function update_lfo()
+  -- Increase counter. Taking sin() from this smooths out rate changes.
+  lfo_counter = lfo_counter + params:get("lfo_rate")
+  lfo_val = params:get("lfo_amount") * math.sin(lfo_counter)
+  
+  -- Set softcut rate.
+  softcut.rate(1, params:get("rate") + lfo_val)
 end
 
 function enc(n, d)
   if n == 1 then
-    rate = util.clamp(rate + d / 100, -3, 3)
-    softcut.rate(1, rate)
+    params:delta("rate", d)
   elseif n == 2 then
-    lfo_rate = util.clamp(lfo_rate + d / 500, 0.01, 0.5)
+    params:delta("lfo_rate", d)
   elseif n == 3 then
-    lfo_amount = util.clamp(lfo_amount + d / 500, 0, 1)
+    params:delta("lfo_amount", d)
   end
-  
-  redraw()
 end
 
 function key(n, z)
-  if n >= 2 and n <= 3 and z == 1 then
-    load_file(n - 1)
+  if n == 2 and z == 1 then
+    load_file(params:get("sample 1"))
+  elseif n == 3 and z == 1 then
+    load_file(params:get("sample 2"))
   end
 end
 
 function redraw()
   screen.clear()
   
-  local radius = 3 + math.abs((rate * 8) + (lfo_val * 2))
+  -- Draw animation.
+  local radius = 3 + math.abs((params:get("rate") * 8) + (lfo_val * 2))
+  
   screen.level(1)
   screen.move(64 + radius, 32)
   screen.circle(64, 32, radius)
   screen.stroke()
   
-  if file1 == "" and file2 == "" then
+  -- Draw help, if needed.
+  if params:get("sample 1") == "-" and params:get("sample 2") == "-" then
     screen.level(5)
     screen.move(0, 10)
     screen.text("^ load a file to start...")
   end
   
+  -- Draw params.
   screen.level(15)
   screen.move(0, 40)
   screen.text("pitch > ")
   screen.level(3)
-  screen.text(string.format("%.2f", rate))
+  screen.text(string.format("%.2f", params:get("rate")))
   screen.move(0, 50)
   screen.level(15)
   screen.text("lfo speed > ")
   screen.level(3)
-  screen.text(string.format("%.2f", lfo_rate))
+  screen.text(string.format("%.2f", params:get("lfo_rate")))
   screen.move(0, 60)
   screen.level(15)
   screen.text("lfo amnt > ")
   screen.level(3)
-  screen.text(string.format("%.2f", lfo_amount))
-  
-  if sample ~= -1 then
-    screen.move(91, 60)
-    screen.level(15)
-    screen.text("samp > ")
-    screen.level(3)
-    screen.text(sample)
-  end
+  screen.text(string.format("%.2f", params:get("lfo_amount")))
   
   screen.update()
 end
